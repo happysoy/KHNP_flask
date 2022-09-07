3a65d5632be2b110b8c79e961093ebb20890b98a
happysoy <jeongsoyeon0130@naver.com>
2022-09-07 20:26:08 +0900

    [feat] 데이터 연동 완려

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
-from flask import Flask
+from flask import Flask, request
 import axisDatas
 app = Flask(__name__)
+app.config['JSON_AS_ASCII']=False
 
-@app.route('/file-datas')
+@app.route('/file-datas', methods=['POST'])
 def home():
+  fileName = request.args['file_name']
+  fileURL = request.args['file_URL']
 
-  result = axisDatas.getData()
+  print("nextjs 로부터 받아온 정보", fileName, fileURL)
+  result = axisDatas.getData(fileName, fileURL)
 
   return result
 
--- axisDatas.py
+++ axisDatas.py
@@ -1,7 +1,7 @@
 import requests
 import json
 
-def getData():
+def getData(fileName, fileURL):
   url = "http://3.34.118.72:5000/echo_call" #ec2 인스턴스의 퍼블릭 ip/탄력적 ip를 써야한다
   data = {"url": 'https://khnp.s3.ap-northeast-2.amazonaws.com/HB-01-23-CD-A1-01-014018.dat',
           "name": 'HB-01-23-CD-A1-01-014018'}
@@ -10,4 +10,4 @@ def getData():
   received = res.content
   convert = json.loads(received.decode("utf-8"))
 
-  return convert['CH3Y']
+  return convert
