192cbc8802eaec5075a0c18e9730b88fa9c5563a
happysoy <jeongsoyeon0130@naver.com>
2022-09-21 23:59:08 +0900

    url 수정

--- axisDatas.py
+++ axisDatas.py
@@ -1,10 +1,11 @@
 import requests
 import json
 
-def getData(fileName, fileURL):
+# 폴더 이름도 받아와야함
+def getData(fileName,fileURL):
   url = "http://3.34.118.72:5000/echo_call" #ec2 인스턴스의 퍼블릭 ip/탄력적 ip를 써야한다
-  data = {"url": 'https://khnp.s3.ap-northeast-2.amazonaws.com/HB-01-23-CD-A1-01-014018.dat',
-          "name": 'HB-01-23-CD-A1-01-014018'}
+  data = {"url": fileURL,
+          "name": fileName}
   res = requests.post(url=url, json=data)
 
   received = res.content
