028a952eb8f5cdb3d933fc1204d54b7d416a7db2
happysoy <jeongsoyeon0130@naver.com>
2022-09-29 19:59:11 +0900

    [feat] flask

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask, request
+from flask import Flask, request, Response
 import requests
 import json
+import time
 
 app = Flask(__name__)
 app.config['JSON_AS_ASCII']=False
@@ -12,6 +13,7 @@ def home():
   tspSetting = request.args['tsp_setting']
   defectSetting = request.args['defect_setting']
 
+  start = time.time();
 
   url = "http://125.6.38.196:5000/echo_call" #ec2 인스턴스의 퍼블릭 ip/탄력적 ip를 써야한다
   data = {"url": fileURL,
@@ -21,12 +23,15 @@ def home():
           }
   res = requests.post(url=url, json=data)
 
-  print("res 끝")
   received = res.content
   convert = json.loads(received.decode("utf-8"))
 
-  return convert
+  end=time.time()
+
+  print("flask 소요 시간 : " , end-start)
+
+  return Response(json.dumps(convert), status=res.status_code, mimetype='application/json')
 
 
 if __name__ == '__main__':
-  app.run(debug=False, host='0.0.0.0')
+  app.run(debug=False, host='0.0.0.0', threaded=True)
